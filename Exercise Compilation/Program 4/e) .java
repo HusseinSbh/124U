f9b517ab6253fa4271decbf2214edef02e317518
HusseinSbh <139717897+HusseinSbh@users.noreply.github.com>
2023-07-21 01:16:10 -0400

    e) .java

--- Exercise Compilation/Program 4/e) .java
+++ Exercise Compilation/Program 4/e) .java
@@ -1,1 +1,14 @@
+public class DivisibleByTwoAndThree {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 1;
 
+        while (count < 20) {
+            if (number % 2 == 0 && number % 3 == 0) {
+                System.out.print(number + " ");
+                count++;
+            }
+            number++;
+        }
+    }
+}
