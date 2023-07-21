9d1dcc2035e2f69903ffac79a89b4c0cc378daff
HusseinSbh <139717897+HusseinSbh@users.noreply.github.com>
2023-07-21 01:04:22 -0400

    Program 1 .java

--- Exercise Compilation/Program 1 .java
+++ Exercise Compilation/Program 1 .java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class ChangeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the change in cents: ");
+        int changeInCents = scanner.nextInt();
+        scanner.close();
+
+        int toonies = changeInCents / 200;
+        changeInCents %= 200;
+
+        int loonies = changeInCents / 100;
+        changeInCents %= 100;
+
+        int quarters = changeInCents / 25;
+        changeInCents %= 25;
+
+        int dimes = changeInCents / 10;
+        changeInCents %= 10;
+
+        int nickels = changeInCents / 5;
+        int pennies = changeInCents % 5;
+
+        System.out.printf("Your change is %d toonie(s), %d loonie(s), %d quarter(s), %d dime(s), %d nickel(s), and %d penny(ies).%n",
+                toonies, loonies, quarters, dimes, nickels, pennies);
+    }
+}
