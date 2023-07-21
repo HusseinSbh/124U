6a89cf2bd76e6dcfa6731a66184a7b609a8e8ceb
HusseinSbh <139717897+HusseinSbh@users.noreply.github.com>
2023-07-21 01:14:03 -0400

    c) .java

--- Exercise Compilation/Program 4/c) .java
+++ Exercise Compilation/Program 4/c) .java
@@ -1,1 +1,14 @@
+public class First50OddNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 1;
 
+        while (count < 50) {
+            if (number % 2 != 0) {
+                System.out.print(number + " ");
+                count++;
+            }
+            number++;
+        }
+    }
+}
