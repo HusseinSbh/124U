d879bf11a4e9fc5308e825a95c4456c4885ea9a8
HusseinSbh <139717897+HusseinSbh@users.noreply.github.com>
2023-07-21 01:15:06 -0400

    d) .java

--- Exercise Compilation/Program 4/d) .java
+++ Exercise Compilation/Program 4/d) .java
@@ -1,1 +1,12 @@
+public class First50EvenNumbers {
+    public static void main(String[] args) {
+        int count = 0;
 
+        for (int i = 1; count < 50; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+    }
+}
